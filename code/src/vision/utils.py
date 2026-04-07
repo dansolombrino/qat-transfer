f3fd536ac9022055f791aa3667275d02eda1d24c
Daniele Solombrino <dansolombrino@gmail.com>
2026-04-07 23:03:21 +0200

    add experiments/000_baselines

--- code/src/vision/utils.py
+++ code/src/vision/utils.py
@@ -13,6 +13,15 @@ def random_tqdm_color():
     return f'#{random.randint(0, 0xFFFFFF):06x}'
 
 
+def accuracy(output, target, topk=(1,)):
+    pred = output.topk(max(topk), 1, True, True)[1].t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    return [
+        float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy())
+        for k in topk
+    ]
+
+
 def sanitize_hf_model_name(model_name: str) -> str:
     """Make a HuggingFace model id safe for use as a single path component.
 
